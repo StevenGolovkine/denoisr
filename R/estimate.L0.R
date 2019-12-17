7d03622a7c913fcd0e7b88f2f0f4aed5e9a1956b
StevenGolovkine <steven_golovkine@icloud.com>
2019-12-17 15:26:30 +0100

    Add smoothing of curves with regularity > 1

--- R/estimate.L0.R
+++ R/estimate.L0.R
@@ -3,98 +3,100 @@
 ###############################################################################
 library(tidyverse)
 
-#' Perform the estimation of L0. 
-#' 
+#' Perform the estimation of L0.
+#'
 #' @param data List of curves to estimate by kernel regression.
 #' @param t0 The starting time for the estimation of H0. We consider the 8k0 - 7
 #'  nearest points of t0 for the estimation of H0 when sigma is unknown.
 #' @param H0 An estimation of H0.
-#' @param k0 For the computation of the gap between the different observations. 
+#' @param k0 For the computation of the gap between the different observations.
 #'  Should be set as k0 = M / max(8, log(M)).
 #' @param sigma True value of sigma.
 #'  If null, change estimate.
-#' @param density Density of the sampling points (currently, only consider 
+#' @param density Density of the sampling points (currently, only consider
 #'  uniform sampling points).
-#'  
+#'
 #' @return An estimation of L0.
-estimate.L0 <- function(data, t0 = 0, H0 = 0, 
-                        k0 = 2, sigma = NULL, density = NULL){
-  
+estimate.L0 <- function(data, t0 = 0, H0 = 0,
+                        k0 = 2, sigma = NULL, density = NULL) {
   S_N <- data
-  
+
   # Estimate mu
-  mu_hat <- S_N %>% map_int(~ length(.x$t)) %>% mean()
-  
+  mu_hat <- S_N %>%
+    map_int(~ length(.x$t)) %>%
+    mean()
+
   # Estimate L0
-  theta <- function(v, k, idx) (v[idx + 2*k - 1] - v[idx + k])**2
-  eta <- function(v, k, idx, H) (v[idx + 2*k - 1] - v[idx + k])**(2*H)
-  
-  nume <- 0; deno <- 1
-  if (is.null(density)) {# Case where the density is not known
-    if (is.null(sigma)) {# Subcase where sigma is not known
+  theta <- function(v, k, idx) (v[idx + 2 * k - 1] - v[idx + k])**2
+  eta <- function(v, k, idx, H) (v[idx + 2 * k - 1] - v[idx + k])**(2 * H)
+
+  nume <- 0
+  deno <- 1
+  if (is.null(density)) { # Case where the density is not known
+    if (is.null(sigma)) { # Subcase where sigma is not known
       idxs <- S_N %>%
-        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(4*k0 - 2)]))
-      a <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = 2*k0 - 1, idx = .y)) %>% 
+        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(4 * k0 - 2)]))
+      a <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = 2 * k0 - 1, idx = .y)) %>%
         mean()
-      b <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>% 
+      b <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>%
         mean()
-      c <- S_N %>% 
-        map2_dbl(idxs, ~ eta(.x$t, k = 2*k0 - 1, idx = .y, H = H0)) %>% 
+      c <- S_N %>%
+        map2_dbl(idxs, ~ eta(.x$t, k = 2 * k0 - 1, idx = .y, H = H0)) %>%
         mean()
-      d <- S_N %>% 
-        map2_dbl(idxs, ~ eta(.x$t, k = k0, idx = .y, H = H0)) %>% 
+      d <- S_N %>%
+        map2_dbl(idxs, ~ eta(.x$t, k = k0, idx = .y, H = H0)) %>%
         mean()
       if ((a - b > 0) & (c - d > 0)) {
         nume <- a - b
         deno <- c - d
       }
-    } else {# Subcase where sigma is known
-      idxs <- S_N %>% 
-        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(2*k0)]))
-      a <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>% 
+    } else { # Subcase where sigma is known
+      idxs <- S_N %>%
+        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(2 * k0)]))
+      a <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>%
         mean()
-      b <- S_N %>% 
-        map2_dbl(idxs, ~ eta(.x$t, k = k0, idx = .y, H = H0)) %>% 
+      b <- S_N %>%
+        map2_dbl(idxs, ~ eta(.x$t, k = k0, idx = .y, H = H0)) %>%
         mean()
-      if ((a - 2*sigma**2 > 0) & b > 0) {
-        nume <- a - 2*sigma**2
+      if ((a - 2 * sigma**2 > 0) & b > 0) {
+        nume <- a - 2 * sigma**2
         deno <- b
       }
     }
-  } else {# Case where the density is known (only the uniform case)
-    if (is.null(sigma)) {# Subcase where sigma is not known
+  } else { # Case where the density is known (only the uniform case)
+    if (is.null(sigma)) { # Subcase where sigma is not known
       idxs <- S_N %>%
-        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(4*k0 - 2)]))
-      a <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = 2*k0 - 1, idx = .y)) %>% 
+        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(4 * k0 - 2)]))
+      a <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = 2 * k0 - 1, idx = .y)) %>%
         mean()
-      b <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>% 
+      b <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>%
         mean()
       if (a - b > 0) nume <- a - b
-      deno <- (2 ** (2 * H0) - 1) * ((k0 - 1) / (mu_hat + 1))**(2 * H0) 
-    } else {# Subcase where sigma is known
-      idxs <- S_N %>% 
-        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(2*k0)]))
-      a <- S_N %>% 
-        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>% 
+      deno <- (2**(2 * H0) - 1) * ((k0 - 1) / (mu_hat + 1))**(2 * H0)
+    } else { # Subcase where sigma is known
+      idxs <- S_N %>%
+        map_dbl(~ min(order(abs(.x$t - t0))[seq_len(2 * k0)]))
+      a <- S_N %>%
+        map2_dbl(idxs, ~ theta(.x$x, k = k0, idx = .y)) %>%
         mean()
-      if (a - 2*sigma**2 > 0) nume <- a - 2*sigma**2
+      if (a - 2 * sigma**2 > 0) nume <- a - 2 * sigma**2
       deno <- ((k0 - 1) / (mu_hat + 1))**(2 * H0)
     }
   }
-  
-  L0_hat <- (nume / deno)**0.5 
-  
+
+  L0_hat <- (nume / deno)**0.5
+
   return(L0_hat)
 }
 
 
 #' Perform the estimation of L0 over a list of t0.
-#' 
+#'
 #' @param data List of curves to estimate by kernel regression.
 #' @param t0_list Starting times for the estimation of H0. We consider the 8k0 - 7
 #'  nearest points of t0 for the estimation of H0 when sigma is unknown.
@@ -104,17 +106,19 @@ estimate.L0 <- function(data, t0 = 0, H0 = 0,
 #'  If null, change estimate.
 #' @param density Density of the sampling points (currently, only consider
 #'  uniform sampling points)?
-#'  
+#'
 #' @return A list containing the estimation of H0 at each t0.
-estimate.L0.list <- function(data, t0_list, H0_list, 
-                             k0 = 2, sigma = NULL, density = NULL){
-  
-  if (length(t0_list) != length(H0_list))
-    stop('t0_list and H0_list must have the same length')
-  
-  L0_hat_list <- t0_list %>% 
-    map2_dbl(H0_list, ~ estimate.L0(data, t0 = .x, H0 = .y, 
-                                    k0 = k0, sigma = sigma, density = NULL))
-  
-  return (L0_hat_list)
+estimate.L0.list <- function(data, t0_list, H0_list,
+                             k0 = 2, sigma = NULL, density = NULL) {
+  if (length(t0_list) != length(H0_list)) {
+    stop("t0_list and H0_list must have the same length")
+  }
+
+  L0_hat_list <- t0_list %>%
+    map2_dbl(H0_list, ~ estimate.L0(data,
+      t0 = .x, H0 = .y,
+      k0 = k0, sigma = sigma, density = NULL
+    ))
+
+  return(L0_hat_list)
 }
--- R/estimate.curve.R
+++ R/estimate.curve.R
@@ -1,17 +1,17 @@
-######################################################################################
-#                     Functions that performs kernel smoothing                       #
-######################################################################################
+################################################################################
+#                 Functions that performs kernel smoothing                     #
+################################################################################
 library(tidyverse)
 
-Rcpp::sourceCpp('./src/estimate_curve.cpp')
+Rcpp::sourceCpp("./src/estimate_curve.cpp")
 
-#' Perform the smoothing of individuals curves
+#' Perform the smoothing of individuals curves.
 #'
 #' @param curve List of two vectors representing a curve:
 #'               - $t Sampling points
 #'               - $x Observed points
 #' @param U Vector of points for the estimation
-#' @param b Bandwith 
+#' @param b Bandwith
 #' @param t0_list List of time at which the bandwidths have been estimated.
 #' @param kernel Kernel name to use
 #'  - epanechnikov (default)
@@ -19,46 +19,48 @@ Rcpp::sourceCpp('./src/estimate_curve.cpp')
 #'  - beta
 #'  - mBeta
 #'  - locPoly (not used)
-#' @param degree Degree of the polynomial for the locpoly function (not used). 
+#' @param degree Degree of the polynomial for the locpoly function (not used).
 #' @useDynLib CovarianceEstimate
-#' 
-#' @return List of two vectors representaing the estimated curve:
+#'
+#' @return List of two vectors representating the estimated curve:
 #'               - $t Sampling points
 #'               - $x Predicted points
-estimate.curve <- function(curve, U, b, t0_list = NULL, 
-                           kernel='epanechnikov', degree = 0){
-  
+estimate.curve <- function(curve, U, b, t0_list = NULL,
+                           kernel = "epanechnikov", degree = 0) {
+
   # Control of the bandwidth parameter
-  if(length(b) == 1){
-    bandwidth = rep(b, length(U))
-  } else if(length(b) != length(U)){
+  if (length(b) == 1) {
+    bandwidth <- rep(b, length(U))
+  } else if (length(b) != length(U)) {
     idx <- U %>% map_dbl(~ order(abs(.x - t0_list))[1])
     bandwidth <- b[idx]
-  } else if(length(b) == length(U)){
-    bandwidth = b
-  } else{
+  } else if (length(b) == length(U)) {
+    bandwidth <- b
+  } else {
     stop("Issues with the bandwidth parameter.")
   }
-  
+
   # Compute the estimation of the curve.
-  if(kernel == 'epanechnikov') {
+  if (kernel == "epanechnikov") {
     x_hat <- epaKernelSmoothingCurve(U, curve$t, curve$x, bandwidth)
-  } else if(kernel == 'uniform') {
+  } else if (kernel == "uniform") {
     x_hat <- uniKernelSmoothingCurve(U, curve$t, curve$x, bandwidth)
-  } else if(kernel == 'beta') {
+  } else if (kernel == "beta") {
     x_hat <- betaKernelSmoothingCurve(U, curve$t, curve$x, bandwidth)
-  } else if(kernel == 'mBeta') {
+  } else if (kernel == "mBeta") {
     x_hat <- modifiedBetaKernelSmoothingCurve(U, curve$t, curve$x, bandwidth)
-  #} else if(kernel == 'locPoly'){
-    #require(KernSmooth)
-    #smooth <- locpoly(curve$t, curve$x, bandwidth = b,
+    # } else if(kernel == 'locPoly'){
+    # require(KernSmooth)
+    # smooth <- locpoly(curve$t, curve$x, bandwidth = b,
     #                 gridsize = 2*length(U), degree = degree)
-    #x_hat <- smooth$y[U == smooth$x]
-  } else{
+    # x_hat <- smooth$y[U == smooth$x]
+  } else {
     print("Wrong kernel name")
     x_hat <- rep(0, length(U))
   }
 
-  return(list(t = U,
-              x = as.vector(x_hat)))
+  return(list(
+    t = U,
+    x = as.vector(x_hat)
+  ))
 }
