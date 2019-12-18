a0cefa04eac313d9c84427f165adea1e0b369ddd
StevenGolovkine <steven_golovkine@icloud.com>
2019-12-18 13:52:29 +0100

    Add estimation of k0

--- R/estimate.k0.R
+++ R/estimate.k0.R
@@ -3,3 +3,24 @@
 ################################################################################
 library(tidyverse)
 
+
+#' Perform the estimation of the pilot k0.
+#' 
+#' @param M Mean number of samping points per curve.
+#' 
+#' @return The pilot k0.
+estimate.k0.pilot <- function(M) {
+  k0_pilot <- trunc((M + 1) * exp(-sqrt(log(M + 1)))) + 1
+  return (k0_pilot)
+}
+
+#' Perform the estimation of the oracle k0.
+#' 
+#' @param M Mean number of sampling points per curve.
+#' @param H Estimation of the regularity of the curves.
+#' 
+#' @return The oracle k0.
+estimate.k0.oracle <- function(M, H) {
+  k0_oracle <- trunc((M + 1)**(H / (2 + H))) + 1
+  return (k0_oracle)
+}
